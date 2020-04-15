aa1d8b6b317a0e8c9756d1696862b5beb430675f
Gabriel Arruda <gda.gabriel@gmail.com>
2020-04-15 00:02:39 -0300

    Using a new text file

--- simulator/app_new.py
+++ simulator/app_new.py
@@ -1,14 +1,25 @@
 import streamlit as st
+import texts_new
 
 if __name__ == '__main__':
 
-    st.sidebar.markdown("# Projeções")
-    st.sidebar.markdown("Selecione as projeções que deseja simular")
-    st.sidebar.markdown("Selecione as projeções que deseja simular")
+    st.sidebar.markdown(texts_new.INTRODUCTION_SIDEBAR)
 
-    w_r0_model = st.sidebar.checkbox("Número de reprodução básico")
-    w_seir_model = st.sidebar.checkbox("Previsão de infectados (modelo SEIR-Bayes)")
-    w_queue_model = st.sidebar.checkbox("Simulação de fila hospitalar")
+    w_r0_model = st.sidebar.checkbox(texts_new.R0_MODEL_DESC)
+    w_seir_model = st.sidebar.checkbox(texts_new.SEIR_MODEL_DESC)
+    w_queue_model = st.sidebar.checkbox(texts_new.QUEUE_MODEL_DESC)
     
-    st.sidebar.markdown("# Seleção de parâmetros")
-g
+    st.sidebar.markdown(texts_new.BASE_PARAMETERS_DESCRIPTION)
+
+    st.sidebar.markdown("**Parâmetro de UF/Município**")
+    st.sidebar.markdown("Unidade")
+    
+    w_location_granularity = st.sidebar.radio(
+        "Unidade",
+        ("Estado", "Município")
+    )
+
+    #if w_location_granularity == 'Estado':
+    #    st.sidebar.selectbox()
+
+
---
+++ simulator/texts_new.py
@@ -0,0 +1,19 @@
+INTRODUCTION_SIDEBAR = '''
+# Projeções
+
+Selecione as projeções que deseja simular.
+
+'''
+
+R0_MODEL_DESC = "Número de reprodução básico"
+SEIR_MODEL_DESC = "Previsão de infectados (modelo SEIR-Bayes)"
+QUEUE_MODEL_DESC = "Simulação de fila hospitalar"
+
+BASE_PARAMETERS_DESCRIPTION = '''
+# Seleção de parâmetros
+
+Para outros cenários, alterer um parâmetro e tecle **Enter**. O novo resultado será calculado e apresentado automaticamente.
+
+---
+'''
+
